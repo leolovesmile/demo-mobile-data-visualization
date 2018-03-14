260521ef882e69f67c7615647ad3b8dbabc886a9
Lan Hao <whu.seven@gmail.com>
2018-03-14 08:44:53 +0800

    create the first little chart

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,41 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as echarts from 'echarts';
 
-import { Item } from '../../models/item';
-import { Items } from '../../providers/providers';
 
 @IonicPage()
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html'
 })
-export class SearchPage {
+export class SearchPage implements AfterViewInit{
+  @ViewChild("chart")
+  chartDiv: any;
 
-  currentItems: any = [];
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
-
-  /**
-   * Perform a service for the proper items.
-   */
-  getItems(ev) {
-    let val = ev.target.value;
-    if (!val || !val.trim()) {
-      this.currentItems = [];
-      return;
-    }
-    this.currentItems = this.items.query({
-      name: val
-    });
+  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  
+  ngAfterViewInit(): void {
+    this.draw();
   }
 
-  /**
-   * Navigate to the detail page for this item.
-   */
-  openItem(item: Item) {
-    this.navCtrl.push('ItemDetailPage', {
-      item: item
-    });
+  draw() {
+    var myChart = echarts.init(this.chartDiv.nativeElement);
+
+    var option = {
+      title: {
+        text: 'ECharts 示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+
+    myChart.setOption(option);
   }
 
 }
